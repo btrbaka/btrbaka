4556c9f219a0815fee6367eac854d577a87eafd6
Eri <eri@nijika.dev>
2024-06-20 17:22:56 +0200

    more error handling in pages + logged in successfully shows school name

--- src/components/Grades.vue
+++ src/components/Grades.vue
@@ -11,57 +11,65 @@ export default {
         async getgrades() {
             const token = localStorage.getItem("token");
             const url = localStorage.getItem("url");
-            let head = { "Content-Type": "application/x-www-form-urlencoded", "Authorization": `Bearer ${token}` }
-            var response = await fetch(`${url}api/3/marks/`, {
-                method: "GET",
-                headers: head
-            })
-            var responseJson = await response.json()
-            // document.write(JSON.stringify(responseJson));
-            var response = Object.values(responseJson);
-            var listDiv = document.getElementById('marksList');
-            let iterator = response.values();
-            for (const value of iterator) {
-                console.log(value);
-            }
-
-            // meoowwwwww nyaaaaa
-            for (let i = 0; i < response[0].length; i++) {
-                const subjectElement = document.createElement("h2");
-                let averagegrade;
-                let temporarygrade;
-                if (response[0][i].AverageText == null || response[0][i].AverageText == " ") {
-                    averagegrade = "N/A";
+            if (localStorage.getItem("url") == null) {
+                alert("Not logged in! Go to the Account page to log in first.")
+            } else {
+                let head = { "Content-Type": "application/x-www-form-urlencoded", "Authorization": `Bearer ${token}` }
+                var response = await fetch(`${url}api/3/marks/`, {
+                    method: "GET",
+                    headers: head
+                })
+                var responseJson = await response.json()
+                if (response.ok == false) {
+                    alert("Authentication failure. Go to the Home tab to refresh login.")
                 } else {
-                    averagegrade = response[0][i].AverageText;
-                }
-                if (response[0][i].TemporaryMark == null || response[0][i].TemporaryMark == " ") {
-                    temporarygrade = "N/A";
-                } else {
-                    temporarygrade = response[0][i].TemporaryMark;
-                }
-                subjectElement.innerHTML =
-                    response[0][i].Subject.Name +
-                    "<br><span> Average grade: " +
-                    averagegrade +
-                    ", </span>" +
-                    "<span> Temporary grade: " +
-                    temporarygrade +
-                    "</span>";
-                listDiv.appendChild(subjectElement);
-                const subjectGrades = document.createElement("details")
-                subjectGrades.innerHTML = "<summary>Tap to see grades... (" + response[0][i].Marks.length + ")</summary>";
-                listDiv.appendChild(subjectGrades);
-                for (let j = response[0][i].Marks.length - 1; j >= 0; j--) {
-                    const subjectGradesItem = document.createElement("li");
-                    subjectGradesItem.innerHTML =
-                        "<span class='gradeText'>" + 
-                        response[0][i].Marks[j].MarkText + "</span> - Weight: " +
-                        response[0][i].Marks[j].Weight + " - " +
-                        "<span class='gradeCaption'>" + 
-                        response[0][i].Marks[j].Caption + "</span>";
+                    // document.write(JSON.stringify(responseJson));
+                    var response = Object.values(responseJson);
+                    var listDiv = document.getElementById('marksList');
+                    let iterator = response.values();
+                    for (const value of iterator) {
+                        console.log(value);
+                    }
 
-                    subjectGrades.appendChild(subjectGradesItem);
+                    // meoowwwwww nyaaaaa
+                    for (let i = 0; i < response[0].length; i++) {
+                        const subjectElement = document.createElement("h2");
+                        let averagegrade;
+                        let temporarygrade;
+                        if (response[0][i].AverageText == null || response[0][i].AverageText == " ") {
+                            averagegrade = "N/A";
+                        } else {
+                            averagegrade = response[0][i].AverageText;
+                        }
+                        if (response[0][i].TemporaryMark == null || response[0][i].TemporaryMark == " ") {
+                            temporarygrade = "N/A";
+                        } else {
+                            temporarygrade = response[0][i].TemporaryMark;
+                        }
+                        subjectElement.innerHTML =
+                            response[0][i].Subject.Name +
+                            "<br><span> Average grade: " +
+                            averagegrade +
+                            ", </span>" +
+                            "<span> Temporary grade: " +
+                            temporarygrade +
+                            "</span>";
+                        listDiv.appendChild(subjectElement);
+                        const subjectGrades = document.createElement("details")
+                        subjectGrades.innerHTML = "<summary>Tap to see grades... (" + response[0][i].Marks.length + ")</summary>";
+                        listDiv.appendChild(subjectGrades);
+                        for (let j = response[0][i].Marks.length - 1; j >= 0; j--) {
+                            const subjectGradesItem = document.createElement("li");
+                            subjectGradesItem.innerHTML =
+                                "<span class='gradeText'>" +
+                                response[0][i].Marks[j].MarkText + "</span> - Weight: " +
+                                response[0][i].Marks[j].Weight + " - " +
+                                "<span class='gradeCaption'>" +
+                                response[0][i].Marks[j].Caption + "</span>";
+
+                            subjectGrades.appendChild(subjectGradesItem);
+                        }
+                    }
                 }
             }
         }
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -27,43 +27,42 @@ export default {
         urlbox.value = localStorage.getItem("url").slice(0, -1);
     },
     methods: {
-            async loginFunc() {
-                const urlbox = document.getElementById("url");
-                const usrnmbox = document.getElementById("usrnm");
-                const pswdbox = document.getElementById("pswd");
-                const rfrshbox = document.getElementById("rfrsh");
-                let url = urlbox.value;
-                const now = new Date();
-                if (url.startsWith("https") == true) {
-                    url = `${url}/`;
-                } else {
-                    url = `https://${url}/`;
-                }
+        async loginFunc() {
+            const urlbox = document.getElementById("url");
+            const usrnmbox = document.getElementById("usrnm");
+            const pswdbox = document.getElementById("pswd");
+            const rfrshbox = document.getElementById("rfrsh");
+            let url = urlbox.value;
+            const now = new Date();
+            if (url.startsWith("https") == true) {
+                url = `${url}/`;
+            } else {
+                url = `https://${url}/`;
+            }
 
-                const usrnm = usrnmbox.value;
-                const pswd = pswdbox.value;
-                const rfrsh = rfrshbox.value;
-                console.log(url);
-                console.log(usrnm);
-                console.log(pswd);
+            const usrnm = usrnmbox.value;
+            const pswd = pswdbox.value;
+            const rfrsh = rfrshbox.value;
+            console.log(url);
+            console.log(usrnm);
+            console.log(pswd);
 
-                let head = { "Content-Type": "application/x-www-form-urlencoded" }
-                if (rfrsh != "") {
-                    body = `client_id=ANDR&grant_type=refresh_token&refresh_token=${rfrsh}`
-                }
-                let body = `client_id=ANDR&grant_type=password&username=${usrnm}&password=${pswd}`
-                var response = await fetch(`${url}api/login/`, {
-                    method: "POST",
-                    headers: head,
-                    body: body
-                })
+            let head = { "Content-Type": "application/x-www-form-urlencoded" }
+            if (rfrsh != "") {
+                body = `client_id=ANDR&grant_type=refresh_token&refresh_token=${rfrsh}`
+            }
+            let body = `client_id=ANDR&grant_type=password&username=${usrnm}&password=${pswd}`
+            var response = await fetch(`${url}api/login/`, {
+                method: "POST",
+                headers: head,
+                body: body
+            })
 
-                var responseJson = await response.json()
-                console.log(response.ok)
-                    if(response.ok == false) {
-                        alert("Login failure. Wrong password? Try again.")
-                    } else {
-                        alert("Logged in successfully!");
+            var responseJson = await response.json()
+            console.log(response.ok)
+            if (response.ok == false) {
+                alert("Login failure. Wrong password? Try again.")
+            } else {
                 let token = responseJson.access_token
                 let refresh = responseJson.refresh_token
                 console.log(token);
@@ -76,6 +75,14 @@ export default {
                     localStorage.setItem("url", url)
                     localStorage.setItem("token", token)
                     localStorage.setItem("refresh", refresh)
+                    let head = { "Content-Type": "application/x-www-form-urlencoded", "Authorization": `Bearer ${token}` }
+                    var response = await fetch(`${url}api/3/user`, {
+                        method: "GET",
+                        headers: head
+                    })
+                    var responseJson = await response.json()
+                    let schoolName = responseJson.SchoolOrganizationName
+                    alert(`Successfully logged into ${schoolName}`);
                 }
             }
         }
