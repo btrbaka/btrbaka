733092be978f4b918e373ff02211272d067a4858
nv ko <linuko@proton.me>
2024-06-22 10:59:52 +0200

    pwa support?

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,47 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: '/btrbaka/'
+    plugins: [
+        vue(),
+        vueJsx(),
+        VitePWA({
+            registerType: 'prompt',
+            injectRegister: false,
+
+            pwaAssets: {
+                disabled: false,
+                config: true,
+            },
+
+            manifest: {
+                name: 'btrbaka',
+                short_name: 'btrbaka',
+                description: 'an alternative webapp for bakalari',
+                theme_color: '#f0f8ff',
+            },
+
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+                cleanupOutdatedCaches: true,
+                clientsClaim: true,
+            },
+
+            devOptions: {
+                enabled: false,
+                navigateFallback: 'index.html',
+                suppressWarnings: true,
+                type: 'module',
+            },
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    base: '/btrbaka/'
 })
