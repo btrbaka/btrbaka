2962c5e4efaabfd0abd841dbd1c3448f6d3383aa
Eri <eri@nijika.dev>
2024-12-27 18:01:28 +0100

    avg grade 2: electric boogaloo

--- src/views/Grades.vue
+++ src/views/Grades.vue
@@ -1,8 +1,10 @@
 <script setup>
 import Grades from "../components/Grades.vue";
+import AvgGrade from "../components/AvgGrade.vue";
 </script>
 
 <template>
     <v-toolbar title="Grades" class="bg-transparent"></v-toolbar>
+    <AvgGrade />
     <Grades />
 </template>
